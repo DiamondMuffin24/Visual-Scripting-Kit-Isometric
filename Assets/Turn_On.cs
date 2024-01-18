7cc496d1fdabf7984abaf24f4a95c77685e3b422
DiamondMuffin24 <146829200+DiamondMuffin24@users.noreply.github.com>
2024-01-18 16:24:07 +0000

    V2
    Includes the link to rhythm scene 1, speech code/Button prompts and basic started map.

---
+++ Assets/Turn_On.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Turn_On : MonoBehaviour
+{
+    public GameObject Dialogue1;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        { 
+         Dialogue1.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            SceneManager.LoadScene("2d_Rhythem_01");
+        }
+    }
+}
---
+++ Assets/character_flipper.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_flipper : MonoBehaviour
+
+{
+    private Sprite HeroKnight;
+    void Start()
+    {
+        HeroKnight= GetComponent<Sprite>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
